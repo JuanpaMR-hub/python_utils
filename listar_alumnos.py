9f537e03503b828ef3783b3568509d746ed7c548
Talleres Puente Alto <tallerespa@inacapmail.cl>
2025-07-07 09:41:39 -0400

    auto crear carpetas

--- listar_alumnos.py
+++ listar_alumnos.py
@@ -22,16 +22,28 @@ def listar_alumnos(path):
     return alumnos
 
 
-def exportar_alumnos_csv(asignatura:str,seccion:str,alumnos:list[str]) -> bool:
-    print(alumnos)
-    with open (f"{seccion}.csv", 'w',encoding="utf-8", newline='') as f:
+def exportar_alumnos_csv(asignatura:str,seccion:str,path:str) -> bool:
+    alumnos = listar_alumnos(path)
+    if "_" in alumnos[0]:
+        cambiar_nombre_carpetas(path)
+        alumnos = listar_alumnos(path)
+    
+    directorio_creado = False
+    while directorio_creado == False:
+        if os.path.exists(f"archivos/csv/{asignatura}"):
+            print("existe csv")
+            directorio_creado = True
+        else:
+            os.mkdir(f"archivos/csv/{asignatura}")
+    
+    with open (f"archivos/csv/{asignatura}/{seccion}.csv", 'w',encoding="utf-8", newline='') as f:
         w = csv.writer(f)
         w.writerow(['Alumno'])
         for alumno in alumnos:
             w.writerow([alumno])
             
             
-exportar_alumnos_csv(asignatura,seccion,listar_alumnos(path))
+exportar_alumnos_csv(asignatura,seccion,path)
         
         
     
