715c7c18a7c6732ac647b9300add153c224f4c8a
Talleres Puente Alto <tallerespa@inacapmail.cl>
2025-07-07 09:22:25 -0400

    first commit

---
+++ listar_alumnos.py
@@ -0,0 +1,37 @@
+import os
+import csv
+os.system('cls')
+
+seccion = "151-3"
+asignatura = "TI3032"
+path = os.path.abspath(f"archivos/{asignatura}/{seccion}")
+
+def cambiar_nombre_carpetas(path):
+    for entrega in os.listdir(path):
+        nombre_alumno = ''.join(entrega.split('_')[0])
+        try:
+            os.rename(os.path.join(path,entrega),os.path.join(path,nombre_alumno))
+        except Exception as e:
+            print(e)
+    return
+
+def listar_alumnos(path):
+    alumnos = []
+    for alumno in os.listdir(path):
+        alumnos.append(alumno)
+    return alumnos
+
+
+def exportar_alumnos_csv(asignatura:str,seccion:str,alumnos:list[str]) -> bool:
+    print(alumnos)
+    with open (f"{seccion}.csv", 'w',encoding="utf-8", newline='') as f:
+        w = csv.writer(f)
+        w.writerow(['Alumno'])
+        for alumno in alumnos:
+            w.writerow([alumno])
+            
+            
+exportar_alumnos_csv(asignatura,seccion,listar_alumnos(path))
+        
+        
+    
